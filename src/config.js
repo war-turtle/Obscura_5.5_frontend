84fc5c96398c66683b2dfb16eb32005ec50287e5
root <root@localhost.localdomain>
2018-09-26 17:52:01 +0530

    starting

--- src/config.js
+++ src/config.js
@@ -1,31 +1,29 @@
 const dev = {
-  api: {
-    url: 'http://localhost:8000',
-  },
-  cypher: {
-    algo: 'aes256',
-    passKey: '394rwe78fudhwqpwriufdhr8ehyqr9pe8fud',
-    encoding: 'hex',
-  },
+    api: {
+        url: "http://0cef828b.ngrok.io"
+    },
+    cypher: {
+        algo: "aes256",
+        passKey: "394rwe78fudhwqpwriufdhr8ehyqr9pe8fud",
+        encoding: "hex"
+    }
 };
 
 const prod = {
-  api: {
-    url: '',
-  },
-  cypher: {
-    algo: 'aes256',
-    passKey: '394rwe78fudhwqpwriufdhr8ehyqr9pe8fud',
-    encoding: 'hex',
-  },
+    api: {
+        url: ""
+    },
+    cypher: {
+        algo: "aes256",
+        passKey: "394rwe78fudhwqpwriufdhr8ehyqr9pe8fud",
+        encoding: "hex"
+    }
 };
 
-const config = process.env.REACT_APP_STAGE === 'production'
-  ? prod
-  : dev;
+const config = process.env.REACT_APP_STAGE === "production" ? prod : dev;
 
 export default {
-  // Add common config values here
-  MAX_ATTACHMENT_SIZE: 5000000,
-  ...config,
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
 };
