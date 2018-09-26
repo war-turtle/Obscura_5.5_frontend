a861cb9b8eeddc4a0b5c09b47b4a5e698e489e02
root <root@localhost.localdomain>
2018-09-26 23:31:21 +0530

    feedback form

--- src/components/ourteam/index.jsx
+++ src/components/ourteam/index.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
+import React from "react";
 
 class Team extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+    constructor(props) {
+        super(props);
+        this.imageArr = [
+            { name: "Name", url: "1.jpg" },
+            { name: "Name", url: "1.jpg" },
+            { name: "Name", url: "1.jpg" },
+            { name: "Name", url: "1.jpg" },
+            { name: "Name", url: "1.jpg" },
+            { name: "Name", url: "1.jpg" }
+        ];
+    }
 
-    };
-  }
+    displayImage(imageArr) {
+        // const url = "images/team/" + imageObj.url;
+        return (
+            <div>
+                {imageArr.map(Obj => (
+                    <div className="col s12 m3">
+                        <img
+                            className="team-img"
+                            src={"images/team/" + Obj.url}
+                        />
+                        <p>{Obj.name}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 
     render = () => (
-      <div className="row center">
-        <div className="col s12">
-          <h5>
-            Meet the team behind Electric Pulp
-          </h5>
-          <div className="row">
+        <div className="row center">
             <div className="col s12">
-                images
+                <h5>Meet the team behind Electric Pulp</h5>
+                <div className="row imageDisplay">
+                    {this.displayImage(this.imageArr)}
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    )
+    );
 }
 
 export default Team;
--- src/components/support/index.jsx
+++ src/components/support/index.jsx
@@ -1,69 +1,107 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import actions from '../../actions';
+import React from "react";
+import { connect } from "react-redux";
+import actions from "../../actions";
 
 class Support extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: '',
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: ""
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
 
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
+    handleSubmit = e => {
+        e.preventDefault();
+        const { sendMessage } = this.props;
+        sendMessage(this.state);
+    };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const { sendMessage } = this.props;
-    sendMessage(this.state);
-  };
+    render = () => {
+        const { message } = this.state;
+        return (
+            <div className="row center">
+                <div className="col s12">
+                    <div className="col s6 offset-s3">
+                        <p className="z-depth-2 card section feedback-card">
+                            <div className="row">
+                                <form
+                                    className="col s12"
+                                    onSubmit={this.handleSubmit}
+                                >
+                                    <div className="row feedback-form">
+                                        <div className="col s12">
+                                            <h4 className="white-text">
+                                                Support
+                                            </h4>
+                                        </div>
+                                        <div className="input-field col s12">
+                                            <textarea
+                                                id="subject"
+                                                name="subject"
+                                                value={message}
+                                                className="materialize-textarea white-text"
+                                                onChange={this.onChange}
+                                            />
+                                            <label
+                                                className="white-text"
+                                                htmlFor="textarea1"
+                                            >
+                                                Subject
+                                            </label>
+                                        </div>
+                                        <div className="input-field col s12 white-text">
+                                            <textarea
+                                                id="object"
+                                                name="object"
+                                                value={message}
+                                                className="materialize-textarea"
+                                                onChange={this.onChange}
+                                            />
+                                            <label
+                                                className="white-text"
+                                                htmlFor="textarea1"
+                                            >
+                                                Object
+                                            </label>
+                                        </div>
+                                    </div>
 
-  render = () => {
-    const { message } = this.state;
-    return (
-      <div className="row center">
-        <div className="col s12">
-          <div className="col s6 offset-s3">
-            <p className="z-depth-5 card section">
-              <div className="row">
-                <form className="col s12" onSubmit={this.handleSubmit}>
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <textarea id="textarea1" name="message" value={message} className="materialize-textarea" onChange={this.onChange} />
-                      <label htmlFor="textarea1">
-                        Textarea
-                      </label>
+                                    <button
+                                        className="btn waves-effect waves-light grey"
+                                        type="submit"
+                                        name="action"
+                                    >
+                                        Submit
+                                        <i className="material-icons right">
+                                            send
+                                        </i>
+                                    </button>
+                                </form>
+                            </div>
+                        </p>
                     </div>
-                  </div>
-
-                  <button className="btn waves-effect waves-light" type="submit" name="action">
-                    Submit
-                    <i className="material-icons right">
-                      send
-                    </i>
-                  </button>
-
-                </form>
-              </div>
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+                </div>
+            </div>
+        );
+    };
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  messageSent: state.messageSent,
+    messageSent: state.messageSent
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  sendMessage: (formData) => {
-    dispatch(actions.sendMessage(formData));
-  },
+    sendMessage: formData => {
+        dispatch(actions.sendMessage(formData));
+    }
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Support);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Support);
