e6f7240fc9f36842981cff31e11f1969888297c4
war-turtle <kartikyadav71199@gmail.com>
2018-09-28 00:03:50 +0530

    team members added

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="page-footer  grey lighten-4 black-text center">
+  <footer className="page-footer  grey lighten-4 black-text center" style={{ position: 'fixed' }}>
     <div className="row">
       <div className="col s12">
         <b>
--- src/components/ourteam/index.jsx
+++ src/components/ourteam/index.jsx
@@ -5,44 +5,75 @@ const DisplayImage = (props) => {
   const { images } = props;
   return (
     <div className="row">
-      <div className="col s12">
-        {images.map((Obj, i) => (
-          <div key={i} className="col s12 m3">
-            <img
-              className="team-img"
-              src={`images/team/${Obj.url}`}
-              alt="team-pic"
-            />
-            <p>
-              {Obj.name}
-            </p>
-          </div>
-        ))}
-      </div>
+
+      {images.map((Obj, i) => (
+        <div key={i} className="col s3">
+          <img
+            className="team-img"
+            src={`images/team/${Obj.url}`}
+            alt="team-pic"
+          />
+          <p>
+            {Obj.name}
+          </p>
+        </div>
+      ))}
+
     </div>
   );
 };
 
 
 const Team = () => {
-  const imagesArray = [
-    { name: 'Name', url: '1.jpg' },
-    { name: 'Name', url: '1.jpg' },
-    { name: 'Name', url: '1.jpg' },
-    { name: 'Name', url: '1.jpg' },
-    { name: 'Name', url: '1.jpg' },
-    { name: 'Name', url: '1.jpg' },
+  const developersArray = [
+    { name: 'Anuj Sharma', url: 'AnujSharma.jpg' },
+    { name: 'Kartik Yadav', url: 'KartikYadav.jpg' },
+    { name: 'Avinash Bharti', url: 'AvinashBharti.jpg' },
+    { name: 'Aryan Kaul', url: 'AryanKaul.jpg' },
+    { name: 'Anshul Malik', url: 'AnshulMalik.jpg' },
   ];
+  const architectArray1 = [
+    { name: 'Sushant Adla', url: 'SushantAdla.jpg' },
+    { name: 'Devayan Mishra', url: 'DevayanMishra.jpg' },
+    { name: 'Prakhar Maheshwari', url: 'PrakharMaheshwari.jpg' },
+    { name: 'Prashant Kumar', url: 'PrashantKumar.jpg' },
+  ];
+
+  const architectArray3 = [
+    { name: 'Rajan Nagpal', url: 'RajanNagpal.jpg' },
+    { name: 'Saqib Kamal', url: 'SaqibKamal.jpg' },
+    { name: 'Saurabh Nandedkar', url: 'SaurabhNandedkar.jpg' },
+    { name: 'Shubhankar', url: 'Shubhankar.jpg' },
+  ];
+
+  const architectArray2 = [
+    { name: 'Tanvi Singla', url: 'TanviSingla.jpg' },
+    { name: 'Vedant Nepal', url: 'VedantNepal.jpg' },
+    { name: 'Abhinav Mishra', url: 'AbhinavMishra.jpg' },
+  ];
+
   return (
-    <div className="row center">
-      <div className="col s12">
-        <h5>
-					Meet the team behind Electric Pulp
-        </h5>
-        <div className="row imageDisplay">
-          <DisplayImage images={imagesArray} />
+    <div>
+      <div className="row center">
+        <div className="col s12">
+          <h3>
+	        Meet the team behind Electric Pulp
+          </h3>
+        </div>
+        <div className="row">
+          <h5 style={{ marginBottom: '50px' }}>
+        Developers
+          </h5>
+          <DisplayImage images={developersArray} />
+          <h5 style={{ marginBottom: '50px' }}>
+        Architects
+          </h5>
+          <DisplayImage images={architectArray1} />
+          <DisplayImage images={architectArray3} />
+          <DisplayImage images={architectArray2} />
         </div>
       </div>
+
     </div>
   );
 };
