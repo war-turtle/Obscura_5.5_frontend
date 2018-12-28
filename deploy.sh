37381b81e3964c106209c0bf5091bc32d346e6f8
war-turtle <kartikyadav71199@gmail.com>
2018-12-28 15:55:44 +0530

    testing

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-snap install kubectl --classic
+sudo snap install kubectl --classic
 # pip install awscli
 # curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-darwin-amd64
 # chmod +x ./kops
