0b14e0849a2d06cab7efafb72b3d28e6c8b1d51e
war-turtle <kartikyadav71199@gmail.com>
2018-12-28 15:18:14 +0530

    testing deployment commands

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ pip install awscli
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-darwin-amd64
 chmod +x ./kops
 sudo mv ./kops /usr/local/bin/
-kops get cluster
+kubectl version
