398c80653f9d207bbec9130b6a5e265f53d5a1b7
war-turtle <kartikyadav71199@gmail.com>
2018-12-28 16:11:22 +0530

    cluster exported from aws

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@ sudo snap install kubectl --classic
 pip install awscli
 wget https://github.com/kubernetes/kops/releases/download/1.10.0/kops-linux-amd64
 chmod +x kops-linux-amd64
-mv kops-linux-amd64 /usr/local/bin/kops
+sudo mv kops-linux-amd64 /usr/local/bin/kops
 kops get cluster
+kops export kubecfg --name ${KOPS_CLUSTER_NAME}
+kubectl cluster-info
