7a07550576052deb23dc170af9ff502e2364301d
war-turtle <kartikyadav71199@gmail.com>
2018-12-28 16:06:36 +0530

    kops command added

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 sudo snap install kubectl --classic
-# pip install awscli
-# curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-darwin-amd64
-# chmod +x ./kops
-# sudo mv ./kops /usr/local/bin/
-kubectl version
+pip install awscli
+wget https://github.com/kubernetes/kops/releases/download/1.10.0/kops-linux-amd64
+chmod +x kops-linux-amd64
+mv kops-linux-amd64 /usr/local/bin/kops
+kops get cluster
