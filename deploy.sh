89b8d4d3d21cc7ded26251da34f32cde950d546c
war-turtle <kartikyadav71199@gmail.com>
2018-12-28 14:52:59 +0530

    test cd on travis ci

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+pip install awscli
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-darwin-amd64
+chmod +x ./kops
+sudo mv ./kops /usr/local/bin/
+kops get cluster
